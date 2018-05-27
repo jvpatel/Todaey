f7de1d0e8f16e50ce9b718853055098b2c8b886a
jvpatel <jvpatel1991@gmail.com>
2018-05-27 13:47:10 -0400

    ToDo list Complete with datasource and delegate methods

--- Todaey/ViewController.swift
+++ Todaey/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy DEMOGORGON"] //TECT FOR CELL1, CELL2, CELL3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK - Tableview DataSource Methods
+    // Display cell and Define number if cells
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count //that many number of rows (cells) in section (view)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //create reusable cell
+        //code sense, makes you know what they mean V: from iboutlet-TableView, L-local from this method
+        //they both refer same thing, "ToDoItemCell" is the identifier we gave upon adding TableViewController in story board, which then gave warning (1)
+        // We got cell dequeud
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row] //every cell has label, current row of current indexpath
+        return cell //reuse prototype cell, this cell be returned and shown as row
+    }
 
+    //MARK - TableView and Delegate Methods
+    // Click cell listening, and TableViewDelegate
+    
+    //check to see if the row is selected
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row) //tells which row was selected
+        print(itemArray[indexPath.row])
+        
+        //cell at this indexpath, will have accessory, set that
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) //shows gray then disappears background
+    }
+    
 }
 
